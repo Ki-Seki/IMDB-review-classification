094fbc6cebfa73617d8202df9c7137247324050d
Ki-Seki <Ki_Seki@outlook.com>
2023-07-14 19:47:17 +0800

    Extract data preprocessing in main.ipynb to IMDBDataset class

---
+++ tests/test_utils_dataset.py
@@ -0,0 +1,61 @@
+import unittest
+import os
+
+import torch
+from torch.utils.data import DataLoader, random_split
+
+from utils.Dataset import IMDBDataset
+
+
+class BasicTest(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super(BasicTest, self).__init__(*args, **kwargs)
+
+        # ! REFACTOR the imdb_dir if you need to
+        imdb_dir = './resources/aclImdb'
+        self.train_dir = os.path.join(imdb_dir, 'train')
+        self.test_dir = os.path.join(imdb_dir, 'test')
+
+    def testTrainValSplit(self):
+        dataset = IMDBDataset(self.train_dir, 100, 10000)
+
+        torch.manual_seed(123)
+        train, valid, _ = random_split(dataset, [200, 10000, len(dataset)-10200])  # split problem
+        self.assertEqual(len(train), 200)
+        self.assertEqual(len(valid), 10000)
+        data1_X, data1_y = train[5]
+
+        torch.manual_seed(123)
+        train, valid, _ = random_split(dataset, [200, 10000, len(dataset)-10200])
+        self.assertEqual(len(train), 200)
+        self.assertEqual(len(valid), 10000)
+        data2_X, data2_y = train[5]
+
+        self.assertTrue(torch.equal(data1_X, data2_X))
+        self.assertEqual(data1_y, data2_y)
+
+    def testUseInDataLoader(self):
+        seq_len = 100
+        vocab_size = 10000
+        batch_size = 32
+        dataset = IMDBDataset(self.train_dir, seq_len, vocab_size)
+        torch.manual_seed(123)
+        train_dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+        seqs, labels = next(iter(train_dataloader))
+        self.assertEqual(seqs.size(), torch.Size([batch_size, seq_len]))
+        self.assertEqual(labels.size(), torch.Size([batch_size]))
+
+
+    def testTokenizerReuse(self):
+        train_dataset = IMDBDataset(self.train_dir, 100, 10000)
+
+        # Reuse the tokenizer in train_dataset
+        test_dataset = IMDBDataset(self.test_dir, 100, 10000, 
+                                   train_dataset.tokenizer)
+
+        self.assertEqual(train_dataset.tokenizer.index_word, test_dataset.tokenizer.index_word)
+        self.assertDictEqual(train_dataset.tokenizer.word_index, test_dataset.tokenizer.word_index)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ utils/Dataset.py
@@ -0,0 +1,61 @@
+import os
+
+import torch
+from torch.utils.data import Dataset
+
+from utils.Tokenizer import Tokenizer
+
+
+class IMDBDataset(Dataset):
+
+    def __init__(self, path: str, seq_len: int, vocab_size: int,
+                 tokenizer: Tokenizer=None):
+        """
+        :param path: Dataset dir
+        :param seq_len: Max len of a seq
+        :param vocab_size: Size of the tokenizer vocabulary
+        :param tokenizer: For tokenizer reuse
+        """
+        self.path = path
+        self.seq_len = seq_len
+        self.vocab_size = vocab_size
+        self.tokenizer = tokenizer
+
+        # Preprocessing
+        texts, labels = self._get_raw_data()
+        seqs = self._tokenization(texts)
+
+        self.seqs = torch.tensor(seqs)
+        self.labels = torch.tensor(labels)
+    
+    def __len__(self):
+        return len(self.seqs)
+    
+    def __getitem__(self, idx):
+        return [self.seqs[idx], self.labels[idx]]
+
+    def _get_raw_data(self):
+        """Read raw data from IMDB files"""
+        texts = []
+        labels = []
+        for label_type in ['neg', 'pos']:
+            dir_name = os.path.join(self.path, label_type)
+            for fname in os.listdir(dir_name):
+                if not fname.endswith('.txt'):
+                    continue
+                with open(os.path.join(dir_name, fname)) as f:
+                    texts.append(f.read())
+                labels.append(0 if label_type == 'neg' else 1)
+        return texts, labels
+    
+    def _tokenization(self, texts):
+        """
+        Conduct tokenization using self.tokenizer if it's None,
+        or using a new tokenizer fitted on the `texts`.
+        :param texts: They will be tokenized
+        :return: Sequences after tokenization
+        """
+        if self.tokenizer is None:
+            self.tokenizer = Tokenizer(self.vocab_size)
+            self.tokenizer.fit_on_texts(texts)
+        return self.tokenizer.texts2seqs(texts, seq_len=self.seq_len)
